b559780010a47aa8505b8863527a9bff6cf4f04f
peterdez <adebanjopete@gmail.com>
2024-03-15 17:24:30 +0100

    Remove ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoansComponent } from './loans/loans.component';
@@ -16,7 +15,6 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
